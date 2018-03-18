688e5b045e849d1691f1026f07959a2386709576
chandu0001 <mail2mechandu@gmail.com>
2018-03-18 20:43:07 +0530

    first commit

---
+++ src/main/java/testjenkins2java.java
@@ -0,0 +1,9 @@
+
+public class testjenkins2java {
+	
+	public String Sample() {
+		return "Sample Program";
+		
+	}
+
+}
---
+++ src/test/java/testjenkins2.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testjenkins2 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
